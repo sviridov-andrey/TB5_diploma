4a57c9ff88e56f4d87be7ecd04362807fc925383
Андрей <chosenfew@yandex.ru>
2024-02-23 02:02:02 +0300

    _

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 import uvicorn
 from create_tables import CreateTables
-from database import cur, conn
 from employees_router import router as employees_router
-from typing import Annotated
+from tasks_router import router as tasks_router
 
-from schemas import EmployeeCreate
 
 create_tables = CreateTables()
 create_tables.create_tables()
 
 app = FastAPI()
 app.include_router(employees_router)
-
-# @app.post("/")
-# def create_employee(employee: Annotated[Employee, Depends()]):
-#
-#     cur.execute(
-#         "INSERT INTO employees (full_name, job_title, email) VALUES (%s, %s, %s) RETURNING id",
-#         (employee.full_name.title(), employee.job_title, employee.email)
-#     )
-#     employee_id = cur.fetchone()[0]
-#     conn.commit()
-#     return {"id": employee_id, **employee.dict()}
+app.include_router(tasks_router)
 
 
 if __name__ == "__main__":
